ec1095b3f2404821824e21cf6636c455451908e4
ay4748 <ay4748@bs.amalnet.k12.il>
2024-01-09 09:27:44 +0200

    dodge

--- app/src/main/java/com/example/imagebutton/MainActivity.java
+++ app/src/main/java/com/example/imagebutton/MainActivity.java
@@ -4,17 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    imageView iV1;
+    ImageView iV1;
+    ImageButton iB1;
+    Random rnd = new Random();
+    int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         iV1= findViewById(R.id.iV1);
+        iB1= findViewById(R.id.iB1);
     }
 
     public void dodge(View view) {
+
 
     }
 }
